2470e773f49773c63f9a2a6c2ae51d20a97c14f1
Adan <twinadanz@gmail.com>
2018-05-23 21:27:42 -0500

    change range names

--- src/components/CustomNavBar.js
+++ src/components/CustomNavBar.js
@@ -15,14 +15,14 @@ class CustomNavbar extends Component {
                     <NavItem eventKey={1} componentClass={Link} href="/" to="/">
                         Home
                     </NavItem>
-                    <NavItem eventKey={3} componentClass={Link} href="/search" to="/search">
+                    <NavItem eventKey={3} componentClass={Link} href="/posts" to="/posts">
                         Search
                     </NavItem>
-                    <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
+                    <NavItem eventKey={2} componentClass={Link} href="/events" to="/events">
                         About
                     </NavItem>
                   </Nav>
-                </Navbar.Collapse>                
+                </Navbar.Collapse>
             </Navbar>
 
         )
