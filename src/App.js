ab6bd02e2387054cffd776641e88d51ccdcb1284
Adan <twinadanz@gmail.com>
2018-05-23 21:04:52 -0500

    fix react router import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 //Components
 import CustomNavbar from './components/CustomNavBar';
