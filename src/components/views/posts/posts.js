74da57e1f434bcb3cea2303e9be214ac09f09f42
Adan <twinadanz@gmail.com>
2018-05-23 21:29:12 -0500

    import posts component

---
+++ src/components/views/posts/posts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+//Actions
+import { fetchPosts } from '../../../actions/postActs'
+
+class Posts extends Component {
+
+  componentWillMount() { // This runs every time this component is called.
+      this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps) { // This runs when props are sent to this component.
+      if(nextProps.newPost) {
+          this.props.posts.unshift(nextProps.newPost); // Add to beginning of array.
+      }
+  }
+
+  render() {
+    const postItems = this.props.posts.map(post => (
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+        </div>
+    ));
+    return (
+    <div>
+      <h1>Posts</h1>
+      {postItems}
+    </div>
+    );
+  }
+}
+
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+    posts: state.posts.items,
+    newPost: state.posts.item
+ })
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
