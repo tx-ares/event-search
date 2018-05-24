d40e8c4fa5e0dd22c0d537ee0d2ce1582ae26676
Adan <twinadanz@gmail.com>
2018-05-23 21:02:03 -0500

    add new reducer to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 // This file acts as a funnel to combine all reducers into one object.
 
 import { combineReducers } from 'redux';
-import postReducer from './postReducer.js';
+import postReducer from './postReducer'; // if there's a reference error, add .js extension
+import eventReducer from './eventReducer'; // if there's a reference error, add .js extension
 
 export default combineReducers({
-    posts: postReducer
+    posts: postReducer,
+    events: eventReducer
 });
